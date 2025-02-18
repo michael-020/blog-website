da415d72fdfd28fb10bed7f452976dc85c23550b
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-19 00:16:56 +0530

    Implemented pagination in blog website
    Signed-off-by: Michael Hosamani <michaelhosamani26@gmail.com>

--- pages/community/[slug].tsx
+++ pages/community/[slug].tsx
@@ -1,236 +1,120 @@
-import { useRouter } from "next/router";
-import ErrorPage from "next/error";
-import Head from "next/head";
-import { GetStaticPaths, GetStaticProps } from "next";
-import Container from "../../components/container";
-import MoreStories from "../../components/more-stories";
-import Header from "../../components/header";
-import PostHeader from "../../components/post-header";
-import SectionSeparator from "../../components/section-separator";
-import Layout from "../../components/layout";
-import PostTitle from "../../components/post-title";
-import Tag from "../../components/tag";
-import {
-  getAllPostsForCommunity,
-  getMoreStoriesForSlugs,
-  getPostAndMorePosts,
-} from "../../lib/api";
-import PrismLoader from "../../components/prism-loader";
-import ContainerSlug from "../../components/containerSlug";
-import { useEffect, useRef, useState } from "react";
-import { useScroll, useSpringValue } from "@react-spring/web";
-import { getReviewAuthorDetails } from "../../lib/api";
-import { calculateReadingTime } from "../../utils/calculateReadingTime";
-import dynamic from "next/dynamic";
-import "./styles.module.css"
+import { useState } from "react";
+import { Post } from "../types/post";
+import { getExcerpt } from "../utils/excerpt";
+import PostPreview from "./post-preview";
+import { FaSearch } from 'react-icons/fa';
+import { getAllPostsForCommunity } from "../lib/api";
 
-const PostBody = dynamic(() => import("../../components/post-body"), {
-  ssr: false,
-});
+export default function MoreStories({
+  posts: initialPosts,
+  isCommunity,
+  isIndex,
+}: {
+  posts: { node: Post }[];
+  isCommunity: boolean;
+  isIndex: boolean;
+}) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [posts, setPosts] = useState(initialPosts);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
 
-const postBody = ({ content, post }) => {
-  const urlPattern = /https:\/\/keploy\.io\/wp\/author\/[^\/]+\//g;
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    setPage(1); // Reset pagination when searching
+    setPosts(initialPosts); // Reset to initial posts
+    setHasMore(true); // Reset hasMore flag
+  };
 
-  const replacedContent = content.replace(
-    urlPattern,
-    `/blog/authors/${post.ppmaAuthorName}/`
+  const loadMorePosts = async () => {
+    if (loading || !hasMore) return;
+    
+    setLoading(true);
+    try {
+      const morePosts = await getAllPostsForCommunity({
+        preview: false,
+        limit: 12,
+        offset: page * 12
+      });
+      if (morePosts?.edges?.length === 0) {
+        setHasMore(false);
+      } else {
+        setPosts(currentPosts => [...currentPosts, ...morePosts.edges]);
+        setPage(p => p + 1);
+      }
+    } catch (error) {
+      console.error('Error loading more posts:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const filteredPosts = posts.filter(({ node }) => 
+    node.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    node.excerpt.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
-  return replacedContent;
-};
-
-export default function Post({ post, posts, reviewAuthorDetails, preview }) {
-  const router = useRouter();
-  const { slug }= router.query;
-  const morePosts = posts?.edges;
-  const [avatarImgSrc, setAvatarImgSrc] = useState("");
-  const time = 5 + calculateReadingTime(post?.content);
-  const [blogWriterDescription, setBlogWriterDescription] = useState("");
-  const [reviewAuthorName, setreviewAuthorName] = useState("");
-  const [reviewAuthorImageUrl, setreviewAuthorImageUrl] = useState("");
-  const [reviewAuthorDescription, setreviewAuthorDescription] = useState("");
-  const [postBodyReviewerAuthor, setpostBodyReviewerAuthor] = useState(0);
-  const [updatedContent, setUpdatedContent] = useState("");
-
-  useEffect(() => {
-    if (reviewAuthorDetails && reviewAuthorDetails?.length > 0) {
-      const authorIndex = post.ppmaAuthorName === "Neha" ? 1 : 0;
-      const authorNode = reviewAuthorDetails[authorIndex]?.edges[0]?.node;
-      if (authorNode) {
-        setpostBodyReviewerAuthor(authorIndex);
-        setreviewAuthorName(authorNode.name);
-        setreviewAuthorImageUrl(authorNode.avatar.url);
-        setreviewAuthorDescription(authorNode.description);
-      }
-    }
-  }, [post, reviewAuthorDetails]);
-  const blogwriter = [
-    {
-      name: post?.ppmaAuthorName || "Author",
-      ImageUrl: avatarImgSrc || "/blog/images/author.png",
-      description: blogWriterDescription || "An author for keploy's blog.",
-    },
-  ];
-  const blogreviewer = [
-    {
-      name: reviewAuthorName || "Reviewer",
-      ImageUrl: reviewAuthorImageUrl || "/blog/images/author.png",
-      description: reviewAuthorDescription || "A Reviewer for keploy's blog",
-    },
-  ];
-
-  const postBodyRef = useRef<HTMLDivElement>();
-  const readProgress = useSpringValue(0);
-  useScroll({
-    onChange(v) {
-      const topOffset = postBodyRef.current?.offsetTop || 0;
-      const clientHeight = postBodyRef.current?.clientHeight || 0;
-      if (v.value.scrollY < topOffset) v.value.scrollY = 0;
-      else if (
-        v.value.scrollY > topOffset &&
-        v.value.scrollY < clientHeight + topOffset
-      ) {
-        v.value.scrollY = ((v.value.scrollY - topOffset) / clientHeight) * 100;
-      } else {
-        v.value.scrollY = 100;
-      }
-      readProgress.set(v.value.scrollY);
-    },
-  });
-  useEffect(() => {
-    if (post && post.content) {
-
-      const content = post.content;
-      const avatarDivMatch = content.match(
-        /<div[^>]*class="pp-author-boxes-avatar"[^>]*>\s*<img[^>]*src='([^']*)'[^>]*\/?>/
-      );
-      console.log(avatarDivMatch ? avatarDivMatch[1] : "No avatar match");
-      if (avatarDivMatch && avatarDivMatch[1]) {
-        setAvatarImgSrc(avatarDivMatch[1]);
-      } else {
-        setAvatarImgSrc("/blog/images/author.png");
-      }
-  
-      // Match the <p> with class pp-author-boxes-description and extract its content
-      const authorDescriptionMatch = content.match(
-        /<p[^>]*class="pp-author-boxes-description multiple-authors-description"[^>]*>(.*?)<\/p>/s
-      );
-      
-      // Apply table responsive wrapper
-      const newContent = content.replace(
-        /<table[^>]*>[\s\S]*?<\/table>/gm,
-        (table) => `<div class="overflow-x-auto">${table}</div>`
-      );
-  
-      setUpdatedContent(newContent);
-
-      if (authorDescriptionMatch && authorDescriptionMatch[1].trim()?.length > 0) {
-        setBlogWriterDescription(authorDescriptionMatch[1].trim());
-      } else {
-        setBlogWriterDescription("An author for Keploy's blog.");
-      }
-    }
-  }, [post]);
-
-  useEffect(() => {
-    if (!router.isFallback && !post?.slug) {
-      router.push("/404"); // Redirect to 404 page if slug is not available
-    }
-  }, [router, router.isFallback, post]);
 
   return (
-    <Layout
-      preview={preview}
-      featuredImage={post?.featuredImage?.node?.sourceUrl || ""}
-      Title={post?.seo.title || "Loading..."}
-      Description={`${post?.seo.metaDesc || "Blog About " + `${post?.title}`}`}
-    >
-      <Header readProgress={readProgress} />
-      <Container>
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <PrismLoader /> {/* Load Prism.js here */}
-            <article>
-              <Head>
-                <title>{`${post?.title || "Loading..."} | Keploy Blog`}</title>
-              </Head>
-              <PostHeader
-                title={post?.title || "Loading..."}
-                coverImage={post?.featuredImage}
-                date={post?.date || ""}
-                author={post?.ppmaAuthorName || ""}
-                categories={post?.categories || []}
-                BlogWriter={blogwriter}
-                BlogReviewer={blogreviewer}
-                TimeToRead={time}
-              />
-            </article>
-          </>
-        )}
-      </Container>
-      <ContainerSlug>
-        {/* PostBody component placed outside the Container */}
-        <div ref={postBodyRef}>
-          <PostBody
-            content={
-              post?.content && postBody({ content: post?.content, post })
-            }
-            authorName={post?.ppmaAuthorName || ""}
-            ReviewAuthorDetails={
-              reviewAuthorDetails &&
-              reviewAuthorDetails?.length > 0 &&
-              reviewAuthorDetails[postBodyReviewerAuthor]
-            }
-            slug={slug}
-          />
+    <section>
+      <h2 className="bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:100%_20px] bg-no-repeat bg-left-bottom w-max mb-8 text-4xl heading1 md:text-4xl font-bold tracking-tighter leading-tight">
+        More Stories
+      </h2>
+      
+      {isIndex && (
+        <div className="flex w-full mb-8">
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="Search posts..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="w-full p-4 pl-10 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+          </div>
         </div>
-      </ContainerSlug>
-      <Container>
-        <article>
-          <footer>
-            {post?.tags?.edges?.length > 0 && <Tag tags={post?.tags} />}
-          </footer>
-          <SectionSeparator />
-          {morePosts?.length > 0 && (
-            <MoreStories isIndex={false} posts={morePosts} isCommunity={true} />
+      )}
+
+      {filteredPosts.length === 0 ? (
+        <p className="text-center text-gray-500">No posts found by the name {`"${searchTerm}"`}</p>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 md:gap-x-8 lg:gap-x-8 gap-y-16 md:gap-y-16 mb-16">
+            {filteredPosts.map(({ node }) => (
+              <PostPreview
+                key={node.slug}
+                title={node.title}
+                coverImage={node.featuredImage}
+                date={node.date}
+                author={node.ppmaAuthorName}
+                slug={node.slug}
+                excerpt={getExcerpt(node.excerpt, 20)}
+                isCommunity={
+                  node.categories.edges[0].node.name === "technology" ? false : true
+                }
+              />
+            ))}
+          </div>
+          
+          {/* CHANGE 6: Add Load More button */}
+          {isIndex && hasMore && !searchTerm && (
+            <div className="flex justify-center mb-8">
+              <button
+                onClick={loadMorePosts}
+                disabled={loading}
+                className="px-6 py-2 bg-orange-500 text-white rounded-full hover:bg-orange-600 disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                {loading ? (
+                  <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white mx-auto" />
+                ) : (
+                  'Load More Posts'
+                )}
+              </button>
+            </div>
           )}
-        </article>
-      </Container>
-    </Layout>
+        </>
+      )}
+    </section>
   );
 }
-
-export const getStaticProps: GetStaticProps = async ({
-  params,
-  preview = false,
-  previewData,
-}) => {
-  const data = await getPostAndMorePosts(params?.slug, preview, previewData);
-  const { communityMoreStories } = await getMoreStoriesForSlugs(data?.post?.tags, data?.post?.slug);
-
-  const authorDetails = [];
-  authorDetails.push(await getReviewAuthorDetails("neha"));
-  authorDetails.push(await getReviewAuthorDetails("Jain"));
-  return {
-    props: {
-      preview,
-      post: data?.post || {},
-      posts: communityMoreStories || [],
-      reviewAuthorDetails: authorDetails || {},
-    },
-    revalidate: 10,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const allPosts = await getAllPostsForCommunity(false);
-  const communtiyPosts =
-    allPosts?.edges
-      .map(({ node }) => `/community/${node?.slug}`) || [];
-  return {
-    paths: communtiyPosts || [],
-    fallback: true,
-  };
-};
